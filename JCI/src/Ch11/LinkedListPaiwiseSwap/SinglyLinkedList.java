45abc3bf611349776a541e26b1747a3745e0528c
qmzo553 <qmzo553@gmail.com>
2023-03-20 23:13:35 +0900

    3.20

--- JCI/src/Ch11/LinkedListPaiwiseSwap/SinglyLinkedList.java
+++ JCI/src/Ch11/LinkedListPaiwiseSwap/SinglyLinkedList.java
@@ -1,5 +1,23 @@
 package Ch11.LinkedListPaiwiseSwap;
 
-public class SinglyLinkedList {
-
+public final class SinglyLinkedList {
+	
+	private final class Node {
+		
+		private int data;
+		private Node next;
+		
+		@Override
+		public String toString() {
+			return " " + data + " ";
+  		}
+	}
+	
+	private Node head;
+	private Node tail;
+	
+	public void insertFirst(int data) {
+		
+		Node newNode = new Node();
+	}
 }
