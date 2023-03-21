c4f10fc8cbebb8d5b8f84c7f6a87f8819e26f6cf
qmzo553 <qmzo553@gmail.com>
2023-03-21 22:08:31 +0900

    3.21

--- JCI/src/Ch11/LinkedListPaiwiseSwap/SinglyLinkedList.java
+++ JCI/src/Ch11/LinkedListPaiwiseSwap/SinglyLinkedList.java
@@ -19,5 +19,65 @@ public final class SinglyLinkedList {
 	public void insertFirst(int data) {
 		
 		Node newNode = new Node();
+		
+		newNode.data = data;
+		newNode.next = head;
+		head = newNode;
+		
+		if(tail == null) {
+			tail = newNode;
+		}
+	}
+	
+	public void swap() {
+		
+		if(head == null || head.next == null) {
+			return;
+		}
+		
+		Node currentNode = head;
+		Node prevPair = null;
+		
+		while (currentNode != null && currentNode.next != null) {
+			
+			Node node1 = currentNode;
+			Node node2 = currentNode.next;
+			Node node3 = currentNode.next.next;
+			
+			Node auxNode = node1;
+			node1 = node2;
+			node2 = auxNode;
+			
+			node1.next = node2;
+			node2.next = node3;
+			
+			if(prevPair == null) {
+				head = node1;
+			} else {
+				prevPair.next = node1;
+			}
+			
+			if(currentNode.next == null) {
+				tail = currentNode;
+			}
+			
+			prevPair = node2;
+			
+			currentNode = node3;
+		}
+	}
+	
+	public void print() {
+
+        System.out.println("\nHead (" + head + ") ----------> Last (" + tail + "):");
+
+        Node currentNode = head;
+        while (currentNode != null) {
+
+            System.out.print(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        System.out.println();
 	}
 }
