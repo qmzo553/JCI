fe19de8b6b6999a97c589acc135c2568ba5fd516
qmzo553 <qmzo553@gmail.com>
2023-03-22 22:27:43 +0900

    3.22

---
+++ JCI/src/Ch11/LinkedListMergeTwoSorted/SinglyLinkedList.java
@@ -0,0 +1,15 @@
+package Ch11.LinkedListMergeTwoSorted;
+
+public final class SinglyLinkedList {
+	
+	private final class Node {
+		
+		private int data;
+		private Node next;
+		
+		@Override
+		public String toString() {
+			return " " + data + " ";
+		}
+	}
+}
