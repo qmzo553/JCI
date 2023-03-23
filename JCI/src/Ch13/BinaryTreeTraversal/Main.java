e7f4e0ba9449dc94b9547a5ab9b318f8082eb221
qmzo553 <qmzo553@gmail.com>
2023-03-23 17:13:40 +0900

    3.23

---
+++ JCI/src/Ch13/BinaryTreeTraversal/Main.java
@@ -0,0 +1,32 @@
+package Ch13.BinaryTreeTraversal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        
+        bt.insert(40);
+        bt.insert(47);
+        bt.insert(45);
+        bt.insert(11);
+        bt.insert(3);
+        bt.insert(44);
+          
+        System.out.println("Root: " + bt.root() + " Size: " + bt.size());
+              
+        System.out.println("\n\nTraversal LEVEL: ");
+        bt.print(BinaryTree.TraversalOrder.LEVEL);
+        
+        System.out.println("\n\nTraversal IN-ORDER: ");
+        bt.print(BinaryTree.TraversalOrder.IN);        
+        
+        System.out.println("\n\nTraversal PRE-ORDER: ");
+        bt.print(BinaryTree.TraversalOrder.PRE);        
+        
+        System.out.println("\n\nTraversal POST-ORDER: ");
+        bt.asList(BinaryTree.TraversalOrder.POST);                        
+	}
+
+}
