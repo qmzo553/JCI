f3eb9c53b25c6f07658ad632da7cc0f2651b4bce
qmzo553 <qmzo553@gmail.com>
2023-03-26 21:38:45 +0900

    3.26

---
+++ JCI/src/Ch15/ScheduleOneElevator/Elevators.java
@@ -0,0 +1,33 @@
+package Ch15.ScheduleOneElevator;
+
+public final class Elevators {
+	
+	private Elevators() {
+		throw new AssertionError("Cannot be instantiated");
+	}
+	
+	public static int time(int k, int floors[]) {
+		
+		if(floors == null || floors.length == 0 || k <= 0) {
+			return -1;
+		}
+		
+		int aux;
+		for(int i = 0; i < floors.length - 1; i++) {
+			for(int j = i + 1; j < floors.length; j++) {
+				if(floors[i] < floors[j]) {
+					aux = floors[i];
+					floors[i] = floors[j];
+					floors[j] = aux;
+				}
+			}
+		}
+		
+		int time = 0;
+		for(int i = 0; i < floors.length; i += k) {
+			time += (2 * floors[i]);
+		}
+		
+		return time;
+	}
+}
---
+++ JCI/src/Ch15/ScheduleOneElevator/Main.java
@@ -0,0 +1,13 @@
+package Ch15.ScheduleOneElevator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int k = 3;
+	     int floors[] = {4, 2, 1, 2, 4};
+	        
+	     System.out.println("Minimum time: " + Elevators.time(k, floors));
+	}
+
+}
